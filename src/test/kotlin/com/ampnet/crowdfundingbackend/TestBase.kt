aca01fc8928764f634c06d129173b7b8343f4dc4
Eugen Druzin <edruzin@gmail.com>
2018-09-25 16:02:36 +0200

    Fix CircleCI build.

--- src/test/kotlin/com/ampnet/crowdfundingbackend/TestBase.kt
+++ src/test/kotlin/com/ampnet/crowdfundingbackend/TestBase.kt
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
@@ -14,6 +15,7 @@ import org.springframework.web.context.WebApplicationContext
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@ActiveProfiles("test")
 abstract class TestBase {
 
     @Autowired
