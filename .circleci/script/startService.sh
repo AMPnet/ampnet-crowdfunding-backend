f0d9bcbe77ff2a66c7e4d8fb5ddea3d2b2fc4c44
Eugen Druzin <edruzin@gmail.com>
2018-10-01 17:50:36 +0200

    Change start service.

--- .circleci/script/startService.sh
+++ .circleci/script/startService.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-start_service() {
-	java -jar crowdfunding-backend.jar & echo $! > ./pid.file
-}
-
-start_service || true
+echo | java -jar crowdfunding-backend.jar & echo $! > ./pid.file &
