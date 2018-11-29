02007899e85b27601d74a23bb335b680ee9534f6
Filip Dujmusic <filip@ampnet.io>
2018-11-29 15:19:59 +0100

    Remove unused imports.

--- src/main/kotlin/com/ampnet/crowdfundingbackend/controller/AuthenticationController.kt
+++ src/main/kotlin/com/ampnet/crowdfundingbackend/controller/AuthenticationController.kt
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
--- src/main/kotlin/com/ampnet/crowdfundingbackend/controller/UserController.kt
+++ src/main/kotlin/com/ampnet/crowdfundingbackend/controller/UserController.kt
@@ -14,7 +14,6 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
