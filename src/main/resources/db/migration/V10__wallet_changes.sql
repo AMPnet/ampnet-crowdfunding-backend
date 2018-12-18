d916a2bfed6789f4e052db28250b514135105aa1
Eugen Druzin <edruzin@gmail.com>
2018-12-18 14:18:56 +0100

    Remove wallet reference from organization table

--- src/main/resources/db/migration/V10__wallet_changes.sql
+++ src/main/resources/db/migration/V10__wallet_changes.sql
@@ -5,4 +5,3 @@ ALTER TABLE wallet ADD COLUMN address VARCHAR(42) UNIQUE NOT NULL;
 ALTER TABLE wallet ADD COLUMN type VARCHAR(8) NOT NULL;
 
 ALTER TABLE app_user ADD COLUMN wallet_id INT REFERENCES wallet(id);
-ALTER TABLE organization ADD COLUMN wallet_id INT REFERENCES wallet(id);
