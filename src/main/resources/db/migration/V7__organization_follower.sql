490e8f8e2d2c93d8257e184ae79a3fdd9f4a23a7
Eugen Druzin <edruzin@gmail.com>
2019-02-14 17:08:48 +0100

    Squash database migrations

--- src/main/resources/db/migration/V7__organization_follower.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE organization_follower (
-    organization_id INT REFERENCES organization(id) NOT NULL,
-    user_id INT REFERENCES app_user(id) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-
-    PRIMARY KEY (organization_id, user_id)
-);
-
-CREATE INDEX idx_org_follower_org_id ON organization_membership(organization_id);
--- src/main/resources/db/migration/V9__organization_invite.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE organization_invite (
-    user_id INT REFERENCES app_user(id) NOT NULL,
-    organization_id INT REFERENCES organization(id) NOT NULL,
-    invited_by INT REFERENCES app_user(id) NOT NULL,
-    role_id INT REFERENCES role(id),
-    created_at TIMESTAMP NOT NULL,
-
-    PRIMARY KEY (organization_id, user_id)
-);
