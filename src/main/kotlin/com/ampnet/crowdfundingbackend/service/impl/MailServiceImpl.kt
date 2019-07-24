915b4117c3a844a9462b7c50adc8f24288787257
Eugen Druzin <edruzin@gmail.com>
2019-07-24 17:25:08 +0200

    Add gRPC client for mail-service

--- src/main/kotlin/com/ampnet/crowdfundingbackend/service/impl/MailServiceImpl.kt
+++ src/main/kotlin/com/ampnet/crowdfundingbackend/service/impl/MailServiceImpl.kt
@@ -1,11 +1,44 @@
 package com.ampnet.crowdfundingbackend.service.impl
 
 import com.ampnet.crowdfundingbackend.service.MailService
+import com.ampnet.mailservice.proto.Empty
+import com.ampnet.mailservice.proto.MailServiceGrpc
+import com.ampnet.mailservice.proto.OrganizationInvitationRequest
+import io.grpc.stub.StreamObserver
+import mu.KLogging
+import net.devh.boot.grpc.client.channelfactory.GrpcChannelFactory
 import org.springframework.stereotype.Service
 
 @Service
-class MailServiceImpl : MailService {
+class MailServiceImpl(
+    private val grpcChannelFactory: GrpcChannelFactory
+) : MailService {
+
+    companion object : KLogging()
+
+    private val mailServiceStub: MailServiceGrpc.MailServiceStub by lazy {
+        val channel = grpcChannelFactory.createChannel("mail-service")
+        MailServiceGrpc.newStub(channel)
+    }
+
     override fun sendOrganizationInvitationMail(to: String, organizationName: String) {
-        // TODO: implement using mail-service
+        val request = OrganizationInvitationRequest.newBuilder()
+                .setTo(to)
+                .setOrganization(organizationName)
+                .build()
+
+        mailServiceStub.sendOrganizationInvitation(request, object : StreamObserver<Empty> {
+            override fun onNext(value: Empty?) {
+                logger.debug { "Successfully sent organization invitation mail to: $to" }
+            }
+
+            override fun onError(t: Throwable?) {
+                logger.warn { "Failed to sent organization invitation mail to: $to. ${t?.message}" }
+            }
+
+            override fun onCompleted() {
+                // successfully sent invitation mail
+            }
+        })
     }
 }
